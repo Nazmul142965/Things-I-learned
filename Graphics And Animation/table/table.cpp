d6b8f37839d55a527d91c38f502f74413480467e
Nazmul Hasan <afnannazmul@gmail.com>
2026-04-04 15:58:23 +0600

    Add 3D table model using OpenGL
    Initial implementation of a 3D table model using OpenGL. Includes cube rendering and basic transformations for table parts.

---
+++ Graphics And Animation/table/table.cpp
@@ -0,0 +1,248 @@
+
+#include<windows.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+
+static int slices = 16;
+static int stacks = 16;
+float degreeX = 0;
+float degreeY = 0;
+float degreeZ = 0;
+
+/* GLUT callback Handlers */
+
+static void resize(int width, int height)
+{
+    const float ar = (float) width / (float) height;
+
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity() ;
+}
+void cube()
+{
+    glBegin(GL_QUADS);
+
+//red front
+    glColor3f(1,0,0);
+    glVertex3f(0.0,1.0,0.0);
+    glVertex3f(1.0,1.0,0.0);
+    glVertex3f(1.0,0.0,0.0);
+    glVertex3f(0.0,0.0,0.0);
+
+    //yellow left
+
+    glColor3f(1,1,0);
+    glVertex3f(0.0,1.0,1.0);
+    glVertex3f(0.0,1.0,0.0);
+    glVertex3f(0.0,0.0,0.0);
+    glVertex3f(0.0,0.0,1.0);
+
+    //blue back
+    glColor3d(0,0,1);
+    glVertex3f(1.0,1.0,1.0);
+    glVertex3f(0.0,1.0,1.0);
+    glVertex3f(0.0,0.0,1.0);
+    glVertex3f(1.0,0.0,1.0);
+
+    //green right
+
+    glColor3f(0,1,0);
+    glVertex3f(1.0,1.0,0.0);
+    glVertex3f(1.0,1.0,1.0);
+    glVertex3f(1.0,0.0,1.0);
+    glVertex3f(1.0,0.0,0.0);
+
+
+
+    //cyan bottom
+
+    glColor3f(1,0,1);
+    glVertex3f(1.0,0.0,1.0);
+    glVertex3f(0.0,0.0,1.0);
+    glVertex3f(0.0,0.0,0.0);
+    glVertex3f(1.0,0.0,0.0);
+
+    //violet top
+
+   glColor3f(0,1,1);
+    glVertex3f(1.0,1.0,0.0);
+    glVertex3f(0.0,1.0,0.0);
+    glVertex3f(0.0,1.0,1.0);
+    glVertex3f(1.0,1.0,1.0);
+    glEnd();
+
+
+}
+
+void table(){
+
+    //table top
+    glPushMatrix();
+    glTranslated(-3, -.5, -6);
+    glScaled(6, 0.5 , 4);
+    cube();
+    glPopMatrix();
+
+    //table leg
+    glPushMatrix();
+    glTranslated(-3, -6, -6);
+    glScaled(0.5, 6 , 0.5);
+    cube();
+    glPopMatrix();
+
+    //right corner
+     glPushMatrix();
+    glTranslated(2.5, -6, -6);
+    glScaled(0.5, 6 , 0.5);
+    cube();
+    glPopMatrix();
+
+    //left corner
+    glPushMatrix();
+    glTranslated(-3, -6, -3);
+    glScaled(0.5, 6 , 0.5);
+    cube();
+    glPopMatrix();
+
+
+    glPushMatrix();
+    glTranslated(2.5, -6, -3);
+    glScaled(0.5, 6 , 0.5);
+    cube();
+    glPopMatrix();
+
+
+
+}
+
+static void display(void)
+{
+    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
+    const double a = t*90.0;
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3d(1,0,0);
+
+
+    glPushMatrix();
+        glTranslated(0,0,-7);
+
+        glTranslated(-3,-0.5,-6);
+        glRotated(degreeX,0.5,0,0);
+        glRotated(degreeY,0,.5,0);
+        glRotated(degreeZ,0,0,.5);
+        glScalef(1,1,1);
+
+        glTranslated(3,0.5,6);
+        table();
+
+
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
+
+
+
+
+static void key(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+        case 27 :
+        case 'q':
+            exit(0);
+            break;
+
+        case 'a':
+            degreeX+=2.5;
+            break;
+
+        case 'b':
+            degreeX-=2.5;
+            break;
+
+        case 'c':
+            degreeY+=2.5;
+            break;
+
+        case 'd':
+            degreeY-=2.5;
+            break;
+
+        case 'e':
+            degreeZ+=2.5;
+            break;
+
+        case 'f':
+            degreeZ-=2.5;
+            break;
+
+        case '-':
+            if (slices>3 && stacks>3)
+            {
+                slices--;
+                stacks--;
+            }
+            break;
+    }
+
+    glutPostRedisplay();
+}
+
+static void idle(void)
+{
+    glutPostRedisplay();
+}
+
+const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
+
+const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
+
+/* Program entry point */
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+    glutCreateWindow("lab 1");
+
+    glutReshapeFunc(resize);
+    glutDisplayFunc(display);
+    glutKeyboardFunc(key);
+    glutIdleFunc(idle);
+
+    glClearColor(1,1,1,1);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+
+
+    glutMainLoop();
+
+    return EXIT_SUCCESS;
+}
+
+
